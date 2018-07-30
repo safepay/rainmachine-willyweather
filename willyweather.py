5772be45023ff36fe05ef5419379975a04de390c
safepay <31983230+safepay@users.noreply.github.com>
2018-07-31 08:23:26 +1000

    Update willyweather.py

--- willyweather.py
+++ willyweather.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2018 Richard Mann
 # All rights reserved.
-# Author: Richard Mann <mann_rj@hotmail.com>
+# Author: Richard Mann <mannrj@outlook.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
